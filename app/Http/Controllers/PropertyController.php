b0ba9d45e45a46bb5eb4ca1120957031b5c5a937
shivam <shivammawai80@gmail.com>
2023-03-07 00:22:16 +0530

    changes

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Http\RedirectResponse; 
+
+use DB;
+use Mail;
+use PHPMailer\PHPMailer;
+use Storage;
+use View;
+use App\Admin;
+// use App\Vender;
+// use App\Hotel;
+// use App\Operator;
+// use App\HotelGallery;
+// use App\HotelAmenity;
+// use App\Amenity;
+// use App\RoomCategory;
+// use App\HotelSeasonRate;
+// use App\PaidAmenity;
+// use App\HotelGroupSeasonRate;
+// use App\Lead;
+// use App\Quotation;
+// use App\Sale;
+// use App\Contacts;
+// use App\AdminRequest;
+// use App\AssignContacts;
+// use App\BulkEmailSendReport;
+// use App\BulkEmailSend;
+use App\EmailTemplat;
+use App\EmailCampaign;
+use App\EmailList;
+use App\SMTPEmail;
+use App\RoomBookedDetails;
+use App\WebsiteEnquiry;
+use App\ModuleMaster;
+use App\MenuMaster;
+use App\CompanyMaster;
+use App\Property;
+use PDF;
+use Validator;
+use Illuminate\Support\Facades\Session;
+
+class PropertyController extends Controller
+{
+	
+
+	public function index(){
+		$property=Property::orderBy('id','DESC')->get();
+		return view('property.index',compact('property'));
+	}
+	// public function log_Admin(){
+	// 	if (session()->exists('comp_ad')) {
+	// 		dd($log_admin);
+	// 	}
+	// }
+	public function create(){
+		// $company=CompanyMaster::get();
+		// $module=ModuleMaster::get();
+		return view('property.create');	
+	}
+	//save property function
+	public function save(Request $request){
+		//dd($request->all());
+		\DB::beginTransaction();
+		try{
+			$validator = \Validator::make($request->all(), [ 
+            	'logo'=>'required',
+            	'property_name'=>'required',
+            	'password'=>'required',
+            	'name'=>'required',
+            ]);
+            if ($validator->fails()) { 
+                return response()->json(['status' => 0, 'msg' => 'Error: '.$validator->errors()->first(), 'data' => '']);
+            }
+            $user=session()->get('admin');
+            if($request->file('logo')){
+             	$name = time().'_'.$request->file('logo')->getClientOriginalName();   
+             	//dd($name);   
+                $destinationPath = public_path('/asset/property_logo');
+                $request->file('logo')->move($destinationPath, $name);
+            }
+			$post=[
+            	'user'	=>	$request->name,
+            	'password'		=>	$request->password,
+            	'property_image'	=> 	(isset($name) ? $name : ''),
+            	'property_name'	=>	$request->property_name,
+            	'user_type'=>'A',
+            	'comp_admin_id'=>$user['id'][0],
+            ];
+		    $save=Property::insert($post);
+            if($save){
+            	\DB::commit();
+            	//dd($save);
+            	return response()->json(['status' => 1, 'msg' => 'Added Successfully', 'data' => '']);
+            }else{
+            	\DB::rollback();
+            	return response()->json(['status' => 0, 'msg' => 'Data not save!', 'data' => '']);
+            }
+		}catch(Exception $e){
+			\DB::rollback();
+            return response()->json(['status' => 0, 'msg' => $e->getMessage(), 'data' => '']);
+		}
+	}
+	public function edit($id){
+		$record=Property::where('id',$id)->first();
+		return view('property.update',compact('record'));
+	}
+	public function update(Request $request){
+		//dd($request->all());
+		\DB::beginTransaction();
+		try{
+            $validator = \Validator::make($request->all(), [ 
+            	// 'logo'=>'required',
+            	'property_name'=>'required',
+            	'password'=>'required',
+            	'name'=>'required',
+            ]);
+            if ($validator->fails()) { 
+                return response()->json(['status' => 0, 'msg' => 'Error: '.$validator->errors()->first(), 'data' => '']);
+            }
+            $user=session()->get('admin');
+            // $count=CompanyMaster::where('')
+            if($request->file('logo')){
+             	$name = time().'_'.$request->file('logo')->getClientOriginalName();   
+             	//dd($name);   
+                $destinationPath = public_path('/asset/property_logo');
+                $request->file('logo')->move($destinationPath, $name);
+            }else{
+            	$prop=Property::where('id',$request->id)->first();
+            	$name = $prop->property_image;
+
+            }
+			$post=[
+            	'user'	=>	$request->name,
+            	'password'		=>	$request->password,
+            	'property_image'	=> 	(isset($name) ? $name : ''),
+            	'property_name'	=>	$request->property_name,
+            	'comp_admin_id'=>$user['id'][0],
+            ];
+		    $save=Property::where('id',$request->id)->update($post);
+            if($save){
+            	\DB::commit();
+            	//dd($save);
+            	return response()->json(['status' => 1, 'msg' => 'Update Successfully', 'data' => '']);
+            }else{
+            	\DB::rollback();
+            	return response()->json(['status' => 0, 'msg' => 'Data not save!', 'data' => '']);
+            }
+            
+		}catch(Exception $e){
+			\DB::rollback();
+            return response()->json(['status' => 0, 'msg' => $e->getMessage(), 'data' => '']);
+		}
+	}
+	public function delete(Request $request){
+		$del=ModuleMaster::where('id',$request->id)->delete();
+		if($del){
+			return response()->json(['status' => 1, 'msg' => 'Data Delete Successfully!', 'data' => '']);
+		}else{
+			return response()->json(['status' => 0, 'msg' => 'Something Went Wrong!', 'data' => '']);
+		}
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Frontend
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// clear cache
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('optimize:clear');
+    return "<h1 style='text-align: center;'>!!Cache cleared successfully!</h1>";
+});
+
+// Get the current database connection info
+Route::get('/artisan-database', function() {
+	try {
+		print_r('databse name= '.DB::connection()->getDatabaseName());
+	} catch (\Exception $e) {
+		die("Could not connect to the database.  Please check your configuration. error:" . $e );
+    }
+});
+
+Route::get('testpdf/', 'ItineraryController@downloadSendQuotation');
+
+Route::get('/viewhotel/{id}', 'CommanController@viewHotel');
+
+
+// Hotel
+
+Route::get('/company_admin', 'SuperAdminController@index');
+Route::get('/company_admin/forget-password', 'SuperAdminController@forgetPassword');
+
+Route::get('/admin/reset-password/{id}', 'SuperAdminController@resetPassword');
+Route::post('/admin/sendMail', 'SuperAdminController@sendMail');
+Route::get('/company_admin/log_admin', 'SuperAdminController@log_Admin');
+Route::post('/company_admin/login', 'SuperAdminController@adminLogin');
+
+Route::get('/company_admin/dashboard', 'SuperAdminController@dashboard');
+Route::get('/company_admin/company-master', 'CompanyMasterController@index');
+Route::get('/company_admin/company-master/create', 'CompanyMasterController@create');
+Route::post('/company_admin/company-master/save', 'CompanyMasterController@save');
+Route::get('/company_admin/company-master/edit', 'CompanyMasterController@edit');
+Route::post('/company_admin/company-master/update', 'CompanyMasterController@update');
+Route::post('/company-master', 'CompanyMasterController@delete');
+Route::get('/company_admin/module-master', 'ModuleMasterController@index');
+Route::get('/module-master/create', 'ModuleMasterController@create');
+Route::post('/company_admin/module-master/save', 'ModuleMasterController@save');
+Route::get('/company_admin/module-master/{id}', 'ModuleMasterController@edit');
+Route::post('/company_admin/module-master/update', 'ModuleMasterController@update');
+Route::post('/company_admin/module-master', 'ModuleMasterController@delete');
+Route::get('/company_admin/menu-master', 'MenuMasterController@index');
+Route::get('/company_admin/menu-master/create', 'MenuMasterController@create');
+Route::post('/company_admin/menu-master/save', 'MenuMasterController@save');
+Route::get('/company_admin/menu-master/{id}', 'MenuMasterController@edit');
+Route::post('/company_admin/menu-master/update', 'MenuMasterController@update');
+Route::post('/company_admin/menu-master', 'MenuMasterController@delete');
+Route::get('/company_admin/property', 'PropertyController@index');
+Route::get('/company_admin/property/create', 'PropertyController@create');
+Route::post('/company_admin/property/save', 'PropertyController@save');
+Route::get('/company_admin/property/{id}', 'PropertyController@edit');
+Route::post('/company_admin/property/update', 'PropertyController@update');
+// clear cache
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('view:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('route:clear');   
+    return "<h1 style='text-align: center;'>Cache cleared successfully !</h1>";
+});
+
+
